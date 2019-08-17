17948f7a61d1ae27f23598a4c02c9e9e01fc18ff
Andrej Snitko <snitko@seznam.cz>
2019-08-17 08:58:34 +0200

    added message templates for creating card fault and creating DB fault

--- src/knards/msg.py
+++ src/knards/msg.py
@@ -1,2 +1,5 @@
 DIVIDER_LINE = '--------------------------------------------------------------\
 --------------------------------------'
+CANNOT_CREATE_CARD = 'Could not store the card in the DB due to an error'
+CANNOT_CREATE_DB = '{} already exists, remove it first (or change the name of \
+DB in config.py), then run this command again'
