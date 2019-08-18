b0f90fa87a4a7eb8b05c9ce073597cc5604908dc
Andrej Snitko <snitko@seznam.cz>
2019-08-18 18:25:16 +0200

    added tests for "main" and "bootstrap-db" CLI subcommands

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,12 +1,31 @@
-import click
 from click.testing import CliRunner
-import sys
 
-from knards import knards
+from knards import knards, config, msg
 
 
 def test_main():
+  """
+  $ kn --help
+  Outputs basic help info on knards. Must contain info about all possible
+  commands.
+  """
   runner = CliRunner()
-  result = runner.invoke(knards.main)
+  with runner.isolated_filesystem():
+    result = runner.invoke(knards.main, ['--help'])
+
   assert result.exit_code == 0
   assert 'Usage' in result.output
+  assert 'bootstrap-db' in result.output
+
+def test_bootstrap_db():
+  """
+  $ kn bootstrap-db
+  Creates a DB with the name specified in the config.py module. If the DB
+  already exists, does nothing and ends silently.
+  """
+  runner = CliRunner()
+  with runner.isolated_filesystem():
+    result = runner.invoke(knards.main, ['bootstrap-db'])
+
+  assert result.exit_code == 0
+  assert result.output == msg.BOOTSTRAP_DB_SUCCESS.format(config.DB) + '\n'
