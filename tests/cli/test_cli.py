7053716149aedd6138348a9e73548401d9928b19
Andrej Snitko <snitko@seznam.cz>
2019-08-19 23:13:51 +0200

    CLI counterpart tests updated: testing the existence of the DB file in test_bootstrap_db(); testing "new" subcommand

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,6 +1,7 @@
+import os
 from click.testing import CliRunner
 
-from knards import knards, config, msg
+from knards import knards, api, config, msg, util
 
 
 def test_main():
@@ -13,9 +14,9 @@ def test_main():
   with runner.isolated_filesystem():
     result = runner.invoke(knards.main, ['--help'])
 
-  assert result.exit_code == 0
-  assert 'Usage' in result.output
-  assert 'bootstrap-db' in result.output
+    assert result.exit_code == 0
+    assert 'Usage' in result.output
+    assert 'bootstrap-db' in result.output
 
 def test_bootstrap_db():
   """
@@ -27,5 +28,70 @@ def test_bootstrap_db():
   with runner.isolated_filesystem():
     result = runner.invoke(knards.main, ['bootstrap-db'])
 
-  assert result.exit_code == 0
-  assert result.output == msg.BOOTSTRAP_DB_SUCCESS.format(config.DB) + '\n'
+    # subcommand returned success
+    assert result.exit_code == 0
+    # subcommand output success message
+    assert result.output == msg.BOOTSTRAP_DB_SUCCESS.format(config.DB) + '\n'
+    # file with the DB exists within the temp dir that was created by test
+    assert os.path.exists(os.getcwd() + '/' + config.DB)
+
+def test_new(mocker):
+  """
+  $ kn new
+  Opens a "question-first" buffer in editor, 
+  """
+  card_obj = knards.Card()
+  question_first_prompt = 'Markers: []\n'
+  question_first_prompt += 'Series: []\n'
+  question_first_prompt += 'No. in series: 1\n'
+  question_first_prompt += msg.DIVIDER_LINE + '\n'
+  question_first_prompt += card_obj.question + '\n'
+  question_first_output = 'Markers: [test markers]\n'
+  question_first_output += 'Series: [test series]\n'
+  question_first_output += 'No. in series: 2\n'
+  question_first_output += msg.DIVIDER_LINE + '\n'
+  question_first_output += 'Test question' + '\n'
+  answer_first_prompt = 'Markers: []\n'
+  answer_first_prompt += 'Series: []\n'
+  answer_first_prompt += 'No. in series: 1\n'
+  answer_first_prompt += msg.DIVIDER_LINE + '\n'
+  answer_first_prompt += card_obj.answer + '\n'
+  answer_first_output = 'Markers: [test markers]\n'
+  answer_first_output += 'Series: [test series]\n'
+  answer_first_output += 'No. in series: 2\n'
+  answer_first_output += msg.DIVIDER_LINE + '\n'
+  answer_first_output += 'Test answer' + '\n'
+
+  runner = CliRunner()
+  with runner.isolated_filesystem():
+    runner.invoke(knards.main, ['bootstrap-db'])
+
+    mocker.patch(
+      'knards.util.open_in_editor',
+      return_value=question_first_output
+    )
+
+    result = runner.invoke(knards.main, ['new'])
+    assert util.open_in_editor.call_count == 2
+    assert util.open_in_editor.call_args_list[0][0][0] == question_first_prompt
+    assert util.open_in_editor.call_args_list[1][0][0] == question_first_output
+
+    assert result.exit_code == 0
+
+    mocker.patch(
+      'knards.util.open_in_editor',
+      return_value=answer_first_output
+    )
+
+    result = runner.invoke(knards.main, ['new', '--af'])
+    assert util.open_in_editor.call_count == 2
+    assert util.open_in_editor.call_args_list[0][0][0] == answer_first_prompt
+    assert util.open_in_editor.call_args_list[1][0][0] == answer_first_output
+
+    assert result.exit_code == 0
+
+    first_obj = api.get_card_by_id(1)
+    assert 'Test question' in first_obj.answer
+
+    second_obj = api.get_card_by_id(2)
+    assert 'Test answer' in second_obj.question
