9c54f95e11fa5d3d8146ad86c716a094987e6d0b
Andrej Snitko <snitko@seznam.cz>
2019-08-22 13:07:35 +0200

    test for get_card_set() documented

--- tests/api/test_crud.py
+++ tests/api/test_crud.py
@@ -58,9 +58,11 @@ def test_create_card_index_generation(init_db):
   card_id = api.create_card(card_obj1, init_db)
   assert card_id == 2
 
-def test_get_card_set_returns_set_of_card_objects(init_db):
+def test_get_card_set_processes_constraints(init_db):
   """
-  TODO
+  Create some cards and try to fetch them out of the DB specifying different
+  constraint sets to see if the method works properly.
+  The method must return a list of objects of type knards.Card
   """
   card_obj1 = knards.Card(
     question='_question_',
@@ -92,8 +94,10 @@ def test_get_card_set_returns_set_of_card_objects(init_db):
   api.create_card(card_obj4, init_db)
 
   result = api.get_card_set(db_path=init_db)
+  # check if method returns a list of knards.Card objects
   for obj in result:
     assert isinstance(obj, knards.Card)
+  # no constraints specified -> returns all cards in the DB
   assert len(result) == 4
   assert result[0].question == '_question_'
   assert result[1].answer == '_answer_'
@@ -106,6 +110,9 @@ def test_get_card_set_returns_set_of_card_objects(init_db):
     revisable_only=True,
     db_path=init_db
   )
+  # only return cards that are ready to be revised
+  # this means cards that either have date_updated == None
+  # or score <= difference of today's date and date_updated in days
   assert len(result) == 2
   assert result[0].score == 0
   assert result[1].score == 20
@@ -114,6 +121,8 @@ def test_get_card_set_returns_set_of_card_objects(init_db):
     today=True,
     db_path=init_db
   )
+  # only return cards that were already revised today
+  # this means cards that have date_updated == today
   assert len(result) == 1
 
   result = api.get_card_set(
@@ -121,11 +130,15 @@ def test_get_card_set_returns_set_of_card_objects(init_db):
     include_markers=['specific'],
     db_path=init_db
   )
+  # returns all cards that have 'specific' within their markers
   assert len(result) == 2
+  # output must not contain question text, only answer
   assert result[0].question == ''
   assert result[1].question == ''
   assert result[0].answer == '_answer_'
   assert result[1].answer == '_answer_'
+  # results must not include cards that have 'nonspecific' within their markers
+  # and not 'specific'; markers are checked word-wise
   assert 'nonspecific' not in result[0].markers
   assert 'nonspecific' not in result[1].markers
 
@@ -136,7 +149,10 @@ def test_get_card_set_returns_set_of_card_objects(init_db):
     exclude_markers=['python'],
     db_path=init_db
   )
+  # only return cards that have 'specific' within their markers and not
+  # 'python'
   assert len(result) == 1
+  # results must not include question or answer texts, only metadata
   assert result[0].question == ''
   assert result[0].answer == ''
   assert result[0].markers == 'javascript specific'
