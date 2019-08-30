6e05b99435be857f01f407c7b7742a8b66c5e2f2
Andrej Snitko <snitko@seznam.cz>
2019-08-30 16:42:58 +0200

    added tests for possible errors with api.bootstrap_db()

--- tests/functional/test_bootstrap_db.py
+++ tests/functional/test_bootstrap_db.py
@@ -1,3 +1,15 @@
-from datetime import datetime, timedelta
-
 from knards import knards, api
+
+
+def test_fail_if_db_path_is_not_string():
+  """
+  bootstrap_db() must fail and return False if passed a non-string argument.
+  """
+  assert api.bootstrap_db(111) is False
+
+def test_fail_in_has_no_permission_to_write():
+  """
+  If bootstrap_db() within the script has no permission to write to a file, it
+  fails and returns False.
+  """
+  assert api.bootstrap_db('/test.db') is False
