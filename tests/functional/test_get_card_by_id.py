c6cd20b3f2a1aef14e217b1ba39976f4921206d9
Andrej Snitko <snitko@seznam.cz>
2019-09-16 06:19:22 +0200

    new test for input arg type + some minor fixes

--- tests/functional/test_get_card_by_id.py
+++ tests/functional/test_get_card_by_id.py
@@ -1,22 +1,32 @@
 from knards import knards, api
 
 
-def test_get_card_by_id_returns_object_of_proper_type_and_has_id_set(init_db):
+def test_card_id_input_arg_must_be_integer(init_db):
+  """
+  get_card_by_id() takes in a card_id input argument, that must be integer.
+  """
+  card_obj = knards.Card()
+  card_id = api.create_card(card_obj, init_db)
+
+  result = api.get_card_by_id('1', init_db)
+  assert result is None
+
+def test_returns_object_of_proper_type_and_has_id_set(init_db):
   """
   get_card_by_id() is expected to return an object of type knards.Card with
   the proper id set (one it was stored with)
   """
   card_obj = knards.Card()
   card_id = api.create_card(card_obj, init_db)
-  return_value = api.get_card_by_id(card_id, init_db)
+  result = api.get_card_by_id(card_id, init_db)
 
-  assert isinstance(return_value, knards.Card)
-  assert return_value.id == card_id
+  assert isinstance(result, knards.Card)
+  assert result.id == card_id
 
-def test_get_card_by_id_with_nonexistent_id(init_db):
+def test_if_nonexistent_id_passed_in_none_is_returned(init_db):
   """
   get_card_by_id() returns None if a card with the passed in id wasn't found
   in the DB.
   """
-  return_value = api.get_card_by_id(1, init_db)
-  assert return_value is None
+  result = api.get_card_by_id(1, init_db)
+  assert result is None
