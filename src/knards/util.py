567cb2fda2736447a5f0c0630682a249786956f5
Andrej Snitko <snitko@seznam.cz>
2019-10-02 20:01:41 +0200

    db_connect() refactored: raising exceptions upon errors, new algorithm

--- src/knards/util.py
+++ src/knards/util.py
@@ -3,37 +3,20 @@ import sqlite3
 import subprocess
 import tempfile
 
-from knards import knards, config, msg
+from knards import knards, config, msg, exceptions
 
 
 def db_connect(db_path):
   """
-  Takes in a DB file path.
-  Returns sqlite connection handler or None upon failure.
+  Check if the DB file exists and, if yes, connect to it and return the
+  connection handler.
   """
-  # connection might fail here if the script has no permission to write to
-  # db_path
-  try:
-    connection = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
-  except sqlite3.OperationalError:
-    print(msg.CANNOT_CONNECT_TO_DB_PERMISSION_DENIED.format(db_path))
-    return None
+  if not os.path.exists(db_path):
+    raise exceptions.DBFileNotFound(
+      'DB file ({}) does not exist.'.format(db_path)
+    )
 
-  cursor = connection.cursor()
-
-  # if DB file doesn't exist, the default behavior is to create it, which is
-  # not we want -> False is returned upon such event
-  try:
-    cursor.execute("""
-      SELECT * FROM cards
-    """)
-  except sqlite3.OperationalError:
-    print(msg.CANNOT_CONNECT_TO_DB_NO_DB.format(db_path))
-    connection.close()
-    os.remove(db_path)
-    return None
-
-  cursor.close()
+  connection = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
   return connection
 
 def open_in_editor(buf, editor='vim'):
